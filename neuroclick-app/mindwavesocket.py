de32e83bc8cadae2fc7e3634d1c898b6c1cee75a
PrabhaMatta <prabha.matta@gmail.com>
2014-03-23 23:49:49 -0700

    added code for writing data to files

--- neuroclick-app/mindwavesocket.py
+++ neuroclick-app/mindwavesocket.py
@@ -9,21 +9,28 @@ from werkzeug.exceptions import NotFound
 import gevent
 from gevent import monkey
 import atexit
+import os
+from os import path
 
 from flask import Flask, Response, request, render_template, url_for, redirect
 
 from pymindwave import headset
 from pymindwave.pyeeg import bin_power
 
+CUR_DIR = os.path.dirname(os.path.realpath(__file__))
+print CUR_DIR
+
+DATA_DIR = None
 
 monkey.patch_all()
 
 app = Flask(__name__)
 app.debug = True
 
-fname = "dummy1.txt"
 FLAG_STATUS = False
-fout = None 
+F_ATT_MED = None 
+F_ALL_DATA = None 
+F_SPECTRUM = None
 
 
 # connect to the headset
@@ -77,7 +84,7 @@ class MindWaveNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
         def send_metrics():
             global hs
             global FLAG_STATUS
-            global fout
+            global F_ATT_MED
             
             while True:
                 t = time.time()
@@ -87,9 +94,11 @@ class MindWaveNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
                 spectrum = raw_to_spectrum(hs.get('rawdata')).tolist()
                 #print spectrum
                 if FLAG_STATUS:
-                    fout.write(str(attention )+ "\t" + str(meditation) +"\t" + str(hs.parser.poor_signal))
-                    print str(attention )+ "\t" + str(meditation) +"\t" + str(hs.parser.poor_signal)
-                    
+                    #print F_ATT_MED
+                    F_ATT_MED.write(str(t) + "\t" + str(attention )+ "\t" + str(meditation) +"\t" + str(hs.parser.poor_signal)+ "\n")
+                    print str(t) + "\t" + str(attention )+ "\t" + str(meditation) +"\t" + str(hs.parser.poor_signal)
+                    F_SPECTRUM.write(str(t) + "\t" + str(spectrum)+"\n")
+                    F_ALL_DATA.write(str(t) + "\t" + str(waves_vector[0] )+ "\t" + str(waves_vector[1]) +"\t" + str(waves_vector[2] )+ "\t" + str(waves_vector[3]) +"\t" + str(waves_vector[4] )+ "\t" + str(waves_vector[5])+ "\t" + str(waves_vector[6])+ "\t" + str(waves_vector[7]) +"\n")
                 self.emit('second_metric', {
                     'timestamp': t,
                     'meditation': {
@@ -167,15 +176,35 @@ def expt():
     """
     return render_template('start.html')
 
-@app.route('/startcall', methods = ["GET"])
+@app.route('/startcall', methods = ["GET","POST"])
 def startbutton():
     """
     show index
     """
-    global fout
+    global CUR_DIR
     global FLAG_STATUS
+    global DATA_DIR
+    global F_ATT_MED
+    global F_SPECTRUM
     
-    fout = open(fname,"w")
+    
+    with open("userids.txt", "a+") as fp:
+        fp.seek(0)
+        last_id = fp.readlines()[-1].strip()
+        print "LAST ID===",last_id
+        next_id = str(int(last_id)+1)
+        fp.write(next_id + "\n") 
+    new_dir = "data/"+str(next_id)
+    print new_dir
+    DATA_DIR = os.path.join(CUR_DIR,new_dir)
+    print DATA_DIR
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)    
+    
+    F_ATT_MED = open(DATA_DIR + "/att_med.txt","w")
+    F_SPECTRUM = open(DATA_DIR + "/spectrum.txt","w")
+    
+    print "AFTER creating...",F_ATT_MED
     FLAG_STATUS = True
     return render_template('expt.html')
 
@@ -191,9 +220,10 @@ def socketio(remaining):
 
  
 def onexit():
-    global fout 
+    global F_ATT_MED 
     print "Exiting..."
-    fout.close()
+    F_ATT_MED.close()
+    F_SPECTRUM.close()
     
 if __name__ == '__main__':
     import atexit
